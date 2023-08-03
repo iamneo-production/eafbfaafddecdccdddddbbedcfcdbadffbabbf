3c6f4feb1155ee9ffb32e4c8d431d4bdd7366f28
Iamneo <techadmin@iamneo.ai>
2023-08-03 18:11:49 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react';
-import { useEffect } from 'react/cjs/react.production.min';
+
 function formatNumber(value) {
 	if (`${value}`.length === 1) {
 		return `0${value}`;
@@ -8,41 +8,73 @@ function formatNumber(value) {
 }
 
 function Stopwatch() {
-	const [hours, setHours] = useState(0);
-	const [minutes, setMinutes] = useState(0);
-	const [seconds, setSeconds] = useState(0);
+	// const [hours, setHours] = useState(0);
+	// const [minutes, setMinutes] = useState(0);
+	// const [seconds, setSeconds] = useState(0);
 	const [showStart, setShowStart] = useState(true);
-    const [timer, setTimer] = useState(0);
-    const ref = useRef();
+	const [showResume, setshowResume] = useState(false);
+	const [timer, setTimer] = useState(0);
+	const ref = useRef();
 
-    function startClickHandler() {
-        setShowStart(false);
-        ref.current = setInterval(() => {
-            setTimer(p => p + 1);
-        }, 1000);
-    }
+	const hours = Math.floor(timer / (60 * 60));
+	const minutes = Math.floor((timer - hours * 60 * 60) / 60);
+	const seconds = Math.floor(timer - hours * 60 * 60 - minutes * 60);
 
-    useEffect(() => {
-        const hours = Math.floor(timer / (60 * 60));
-        const minutes = Math.floor(timer / 60);
-        const seconds = Math.floor(timer / (60 * 60));
-    }, [timer]);
+	function startClickHandler() {
+		setShowStart(false);
+		setshowResume(false);
+		ref.current = setInterval(() => {
+			setTimer((p) => p + 1);
+		}, 1000);
+	}
 
-    function resetClickHandler() {
-        setShowStart(true);
-    }
+	function pauseClickHandler() {
+		setshowResume(true);
+		clearInterval(ref.current);
+	}
+
+	function resetClickHandler() {
+		setShowStart(true);
+		setshowResume(false);
+		setTimer(0);
+		clearInterval(ref.current);
+		ref.current = null;
+	}
+
+	function resumeClickHandler() {
+		setshowResume(false);
+		ref.current = setInterval(() => {
+			setTimer((p) => p + 1);
+		}, 1000);
+	}
 
 	return (
-		<div ref={ref}>
+		<div>
 			<p data-testid="time">
 				{formatNumber(hours)}:{formatNumber(minutes)}:{formatNumber(seconds)}
 			</p>
 			{showStart ? (
-				<button data-testid="start" onClick={startClickHandler}>Start</button>
+				<button data-testid="start" onClick={startClickHandler}>
+					Start
+				</button>
 			) : (
-				<button data-testid="pause">Pause</button>
+				<div>
+					{showResume ? (
+						<button data-testid="resume" onClick={resumeClickHandler}>
+							Resume
+						</button>
+					) : (
+						<button data-testid="pause" onClick={pauseClickHandler}>
+							Pause
+						</button>
+					)}
+				</div>
 			)}
-			<button data-testid="reset" onClick={resetClickHandler} disabled={showStart}>
+			<button
+				data-testid="reset"
+				onClick={resetClickHandler}
+				disabled={showStart}
+			>
 				Reset
 			</button>
 		</div>
