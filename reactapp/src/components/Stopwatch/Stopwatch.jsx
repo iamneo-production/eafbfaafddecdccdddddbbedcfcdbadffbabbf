fa993138e3aee7d5bdd9360d66d4705f8b33774b
Iamneo <techadmin@iamneo.ai>
2023-08-03 17:36:52 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
 function Stopwatch() {
-    return (<></>);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [showStart, sets] = useState(false);
+
+    return (
+        <div>
+            <p data-testId="time">{hours}:{minutes}:{seconds}</p>
+            {<button data-testId="start">Start</button>
+            <button data-testId="pause">Pause</button>
+            <button data-testId="reset">Reset</button>
+        </div>
+    );
 }
 
 export default Stopwatch;
