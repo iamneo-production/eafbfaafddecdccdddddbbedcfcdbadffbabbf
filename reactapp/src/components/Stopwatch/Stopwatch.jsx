0e33fc013ebdee9a1b2b1d1e55a3195498da0664
Iamneo <techadmin@iamneo.ai>
2023-08-03 17:51:50 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { useEffect } from 'react/cjs/react.production.min';
 function formatNumber(value) {
 	if (`${value}`.length === 1) {
 		return `0${value}`;
@@ -11,17 +12,28 @@ function Stopwatch() {
 	const [minutes, setMinutes] = useState(0);
 	const [seconds, setSeconds] = useState(0);
 	const [showStart, setShowStart] = useState(true);
+    const [timer, setTimer] = useState(0);
+    const ref = useRef();
 
     function startClickHandler() {
         setShowStart(false);
+        ref.current = setInterval(() => {
+            setTimer(p => p + 1);
+        }, 1000);
     }
+
+    useEffect(() => {
+        const hours = Math.floor(timer / (60 * 60));
+        const minutes = Math.floor(timer / 60);
+        const seconds = Math.floor(timer / (60 * 60));
+    }, [timer]);
 
     function resetClickHandler() {
         setShowStart(true);
     }
 
 	return (
-		<div>
+		<div ref={ref}>
 			<p data-testid="time">
 				{formatNumber(hours)}:{formatNumber(minutes)}:{formatNumber(seconds)}
 			</p>
