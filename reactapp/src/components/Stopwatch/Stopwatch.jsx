1d596d6f4e6909092b8e052087b2596e1a2bbf87
Iamneo <techadmin@iamneo.ai>
2023-08-03 17:46:49 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react';
+function formatNumber(value) {
+	if (`${value}`.length === 1) {
+		return `0${value}`;
+	}
+	return value;
+}
+
 function Stopwatch() {
 	const [hours, setHours] = useState(0);
 	const [minutes, setMinutes] = useState(0);
 	const [seconds, setSeconds] = useState(0);
 	const [showStart, setShowStart] = useState(true);
 
+    function startClickHandler() {
+        setShowStart(false);
+    }
+
+    function resetClickHandler() {
+        setShowStart(true);
+    }
+
 	return (
 		<div>
-			<p data-testId="time">
-				{hours}:{minutes}:{seconds}
+			<p data-testid="time">
+				{formatNumber(hours)}:{formatNumber(minutes)}:{formatNumber(seconds)}
 			</p>
 			{showStart ? (
-				<button data-testId="start">Start</button>
+				<button data-testid="start" onClick={startClickHandler}>Start</button>
 			) : (
-				<button data-testId="pause">Pause</button>
+				<button data-testid="pause">Pause</button>
 			)}
-			<button data-testId="reset">Reset</button>
+			<button data-testId="reset" onClick={resetClickHandler} disabled={showStart}>
+				Reset
+			</button>
 		</div>
 	);
 }
